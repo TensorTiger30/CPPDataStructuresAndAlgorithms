5425fd812830440c15305e6907be311e4bc632cb
Partho Ghosh <ghoshparth.96@gmail.com>
2026-08-23 18:55:13 +0530

    [feat] implement basic structure

--- Structure/structOne.cpp
+++ Structure/structOne.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+struct Rectangle {
+    int length;
+    int breadth;
+};
+
 int main() {
-    cout << "Hello World!";
+
+    struct Rectangle r;
+    int operation;
+
+    cout << "Enter the lenght of the rectangle:";
+    cin >> r.length;
+
+    cout << "Enter the breadth of the rectangle:";
+    cin >> r.breadth;
+
+    cout << "What you want to calculate for rectangle (1 -> Perimeter/ 2 -> Area) ?";
+    cin >> operation;
+
+    switch (operation) {
+        case 1 : cout << "Perimeter of rectangle:" << 2 * (r.length + r.breadth);
+            break; 
+        case 2 : cout << "Area of rectangle:" << (r.length * r.breadth); 
+            break;
+        default:
+            cout<< "Invalid operation requested";
+    }
+
     return 0;
 }
