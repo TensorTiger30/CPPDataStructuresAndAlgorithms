e9a911d415095cf13ff5bf4f8191ac107862f662
Partho Ghosh <ghoshparth.96@gmail.com>
2026-08-29 21:52:47 +0530

    [feat] add function to return struct as pointer

--- Reference/main.cpp
+++ Reference/main.cpp
@@ -23,6 +23,19 @@ void funByReference(struct Rectangle *r) {
     cout << "Breadth: " << r -> breadth << endl;
 }
 
+// get a pointer to a reactangle
+struct  Rectangle *getPointerToAStruct() {
+    cout << "Inside getPointerToAStruct()" << endl;
+    // struct Rectangle *r = (struct Rectangle *) malloc(sizeof(struct Rectangle)); // C syntax
+    struct Rectangle *r = new Rectangle(); // C++ syntax
+
+    // set the values
+    r -> length = 50;
+    r -> breadth = 60;
+
+    return r;
+}
+
 int main() {
     cout << "Inside main()" << endl;
     struct Rectangle r = {10, 2};
@@ -31,5 +44,8 @@ int main() {
     funByValue(r);
     funByReference(&r);
     funByValue(r);
+    struct  Rectangle *p = getPointerToAStruct();
+    cout << "Length: " << p -> length << endl;
+    cout << "Breadth: " << p -> breadth << endl;
     return 0;
 }
